128b63133211ea578663357ac18703ade663c2bf
Brian <brian@bitopia.digital>
2022-12-06 10:25:05 -0300

    first commit with sass

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return (<div className="footer">
+        hola soy el footer
+    </div>)
+}
+
+export default Footer
---
+++ components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import styles from './header.module.scss'
+
+const Header = () => {
+  return <div className={styles.header}>hola soy el Header</div>
+}
+
+export default Header
